1e434c6a06029bff8035653bf27b399a80012c20
oasis791 <khwi791@naver.com>
2024-12-14 23:42:24 +0900

    stg2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,8 +7,6 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-
-        if(input.equals("invalid_command"))
-            System.out.println(input + ": command not found");
+        System.out.println(input + ": command not found");
     }
 }
